8615d801e1de3f0834093dc814724c7cceed0094
sielski yannick <sielskiyannick@gmail.com>
2018-07-30 23:29:31 +0200

    initial web api project commit

---
+++ Portfolio_API/PortfolioAPI/Common/Core/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Core
+{
+    public abstract class EntityBase<TId>
+    {
+        public TId Id { get; set; }
+    }
+}
---
+++ Portfolio_API/PortfolioAPI/Domain/Entities/CurriculumVitae.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Core;
+
+namespace Domain.Entities
+{
+    public class CurriculumVitae: EntityBase<Guid>
+    {
+        public string TestText { get; set; }
+    }
+}
