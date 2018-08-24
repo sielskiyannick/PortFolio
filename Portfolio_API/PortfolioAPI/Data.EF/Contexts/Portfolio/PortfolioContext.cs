db883f1710b8e3148a3520112f07517f3bdad489
Yannick Sielski <yannicks@westmeerbeek.be>
2018-08-24 16:52:39 +0200

    correctie checkin

---
+++ Portfolio_API/PortfolioAPI/Data.EF/Contexts/Portfolio/PortfolioContext.cs
@@ -0,0 +1,28 @@
+﻿using Data.EF.Contexts.Portfolio.Configurations;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.EF.Contexts.Portfolio
+{
+    public class PortfolioContext : DbContext
+    {
+        public virtual DbSet<CV> CVs { get; set; }
+
+        public PortfolioContext(DbContextOptions<PortfolioContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //to do: service resolver (/locator ?)
+            var cvConfig = new CVConfiguration();
+            var cvEntryConfig = new CVEntryConfiguration();
+
+            cvConfig.Configure(modelBuilder);
+            cvEntryConfig.Configure(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
